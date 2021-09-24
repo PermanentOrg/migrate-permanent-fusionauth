fa97face4561cc6336346151f3281758608160a2
Jason Owen <jason@jasonaowen.net>
2021-09-24 16:59:15 -0700

    Parse command line arguments and save JSON to file
    I think we will want to do a few different things in this script, so use
    yargs[1] to parse command line arguments and invoke commands. As a first
    command, extract & transform the data and write it to the file name
    provided.
    
    [1] https://www.npmjs.com/package/yargs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './env';
 
+import { promises as fs } from 'fs';
 import { requireEnv } from 'require-env-variable';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 
 import { extract } from './extract';
 import { logger } from './log';
@@ -8,12 +11,24 @@ import { transform } from './transform';
 
 const { DATABASE_URL } = requireEnv('DATABASE_URL');
 
-const main = () => extract(DATABASE_URL)
-  .then(transform)
-  .then((users) => logger.info('Transformed', { users }))
-;
+const main = async (argv: string[]) => yargs(hideBin(argv))
+  .version(false)
+  .command(
+    'save <filename>',
+    'Extract & transform the data, then write it to a file',
+    (y) => y.positional('filename', {
+      describe: 'file to write JSON data to',
+    }).normalize('filename'),
+    async ({ filename }) => {
+      const data = await extract(DATABASE_URL).then(transform);
+      await fs.writeFile(filename as string, JSON.stringify(data, null, 2), 'utf8');
+      logger.info(`Wrote ${data.length} users to ${filename}`);
+    },
+  )
+  .demandCommand()
+  .parse();
 
-main()
+main(process.argv)
   .then(() => logger.info('done'))
   .catch((err) => {
     logger.error(err);
